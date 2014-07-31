e5cc47b99f208a7080c00577f76bb568d45054a6
Igor Pomaranskiy <ipomaranskiy@gmail.com>
2014-07-31 17:31:21 +0300

    Added workaround for the case, when user changes his username at GitHub.

--- socialregistration/contrib/github/views.py
+++ socialregistration/contrib/github/views.py
@@ -1,6 +1,6 @@
 from django.core.urlresolvers import reverse
 from socialregistration.contrib.github.client import Github
-from socialregistration.contrib.github.models import GithubProfile
+from socialregistration.contrib.github.models import GithubProfile, GithubAccessToken
 from socialregistration.views import OAuthRedirect, OAuthCallback, SetupCallback
 
 class GithubRedirect(OAuthRedirect):
@@ -20,5 +20,14 @@ class GithubSetup(SetupCallback):
     template_name = 'socialregistration/github/github.html'
     
     def get_lookup_kwargs(self, request, client):
-        return {'github': client.get_user_info()['login']}
-    
+        login = client.get_user_info()['login']
+        # workaround for the case, when user changes his username at GitHub
+        try:
+            old_github_profile = GithubAccessToken.objects.get(access_token=client._access_token).profile
+            old_github_profile.github = login
+            old_github_profile.save(force_update=True)
+        except GithubAccessToken.DoesNotExist:
+            pass
+        # end of workaround
+        return {'github': login}
+
